474763313fa9c09c6cc6c16a0be6499662f44bf2
PBJ <dlthasd123@gachon.ac.kr>
2025-07-04 20:34:23 +0900

    Title: 두 수의 거듭제곱, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250704/두 수의 거듭제곱/two-numbers-of-squares.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    // Please write your code here.
+
+    printf("%d", (int)pow(a,b));
+
+
+    return 0;
+}
